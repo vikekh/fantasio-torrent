87958f52ae361b4410dd4c436720bdb6a0a98034
vikekh <621212+vikekh@users.noreply.github.com>
2021-10-01 21:04:02 +0200

    Backup

---
+++ scripts/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
+
+if [ -z "$1" ]; then
+    echo "No argument"
+    exit 1
+fi
+
+# DATE=$(date '+%Y-%m-%d')
+# BACKUP_DIR="${1}/${DATE}"
+BACKUP_DIR=$1
+mkdir $BACKUP_DIR
+# rm -r "${BACKUP_DIR}"/*
+
+. $2
+
+docker stop $TRANSMISSION_CONTAINER_NAME
+docker stop $TRANSMISSION_RSS_CONTAINER_NAME
+
+docker run --rm -v $TRANSMISSION_CONFIG_VOLUME_NAME:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/$TRANSMISSION_CONFIG_VOLUME_NAME.tar.gz /data
+docker run --rm -v $TRANSMISSION_DATA_VOLUME_NAME:/data -v $BACKUP_DIR:/backup ubuntu tar czvf /backup/$TRANSMISSION_DATA_VOLUME_NAME.tar.gz /data
+
+docker start $TRANSMISSION_CONTAINER_NAME
+docker start $TRANSMISSION_RSS_CONTAINER_NAME
